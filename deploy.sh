d047e31dd3a906a8aa949fb4dc77685f3a2a4c2d
Timo Hofmeijer <t@hofmeijer.me>
2016-02-07 22:04:59 +0100

    deploy tweaks

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,16 @@ echo "deploying..."
 set -e
 GITURL=`git config remote.origin.url`
 npm install
+
 ENV_PROD=true metalsmith
 cd build
+# temporarily unignore /build? seems not needed
+# to ignore at it all with this script :)
+#sed -i '' 's/#\/build/\/build/g' .gitignore
 rm -rf .git/
 git init
 git remote add origin $GITURL
-echo $GITURL
 git add .
 git commit -am "deploy"
 git push origin master:gh-pages --force
-echo "deployed"
+echo "done"
