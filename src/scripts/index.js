dbd6fbdd3ac454eecc25a6ad71d1f20e2afde1f2
Timo Hofmeijer <t@hofmeijer.me>
2016-04-24 01:54:05 +0200

    Added svg icons and sprite styles. Refactored front page markup and styling. Added basic video behavior. Added `jQuery` and `bigscreen.js` (last unused still) Added postcss plugins (props & calc)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,58 @@
+$(function() {
+  // Init
+  $('.MovieProject').each(function(i, project) {
+    var $project = $(project);
+    var vimeoId = $project.data('vimeo-id');
+    var iframe = document.querySelector('#vimeoplayer'+vimeoId);
+    var player = $f(iframe);
 
-var toggleActive = function(e) {
-  var target = e.target;
-  var parent = target.parentNode;
-  var children = Array.prototype.slice.call(parent.children);
-  children.forEach(function(n) {
-    n.classList.remove('is-active');
+    player.addEvent('ready', function() {
+
+      player.addEvent('pause', function() {
+        updateState($project, 'pause');
+      });
+      player.addEvent('play', function() {
+        updateState($project, 'play');
+      });
+      // player.addEvent('finish', onFinish);
+      // player.addEvent('playProgress', onPlayProgress);
+      //
+      // Play movie
+      // debugger
+
+    });
+
+    $project.find('.Icon--play').on('click', function() {
+      player.api('play');
+      updateState($project, 'play');
+    });
+
+    $project.find('.Icon--pause').on('click', function() {
+      player.api('pause');
+      updateState($project, 'pause');
+    });
   });
-  target.classList.add('is-active');
-}
+});
 
-var setStillMode = function(e) {
-  toggleActive(e);
 
-}
-var setVideoMode = function(e) {
-  toggleActive(e);
+var updateState = function($project, state) {
+  if (state === 'play') {
+    $project.find('.Icon--play').hide();
+    $project.find('.Icon--text').hide();
+    $project.find('.Icon--pause').show();
 
-}
-var setTextMode = function(e) {
-  toggleActive(e);
+    $project.find('.MovieProject-still').hide();
 
-}
+    // TODO
+    $project.find('.Icon--fullscreenEnter').show();
+    $project.find('.Icon--fullscreenExit').hide();
+  } else if (state === 'pause') {
+    $project.find('.Icon--play').show();
+    $project.find('.Icon--text').show();
+    $project.find('.Icon--pause').hide();
+    
+    // TODO
+    $project.find('.Icon--fullscreenEnter').hide();
+    $project.find('.Icon--fullscreenExit').hide();
+  }
+};
